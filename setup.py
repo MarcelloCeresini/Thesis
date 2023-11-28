bd7c4c3c8e421e0143bb221cbc0afb83c53c16d6
MarcelloCeresini <mceresini.97@gmail.com>
2023-11-28 16:23:04 +0100

    completed setup file and added directories to gitignore

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ import subprocess
 VENV_NAME = "env_" + os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 
 subprocess.run(["python3", "-m", "venv", VENV_NAME])
-subprocess.run(["source", VENV_NAME + "/bin/activate"])
-subprocess.run(["pip", "install", "-r", "requirements.txt"])
-
-
+subprocess.run([f"{VENV_NAME}/bin/python3", "-m", "pip", "install", "--upgrade", "pip"])
+subprocess.run([f"{VENV_NAME}/bin/python3", "-m", "pip", "install", "-r", "requirements.txt"])
